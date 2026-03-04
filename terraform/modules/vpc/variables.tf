66e0a6d61ceaecbe3045192d75b335fd3e475085
Ganil151 <ganilbatistyan@gmail.com>
2026-03-04 16:04:47 -0400

    vpc, project assignment,obsidian

---
+++ terraform/modules/vpc/variables.tf
@@ -0,0 +1,29 @@
+variable "project_name" {
+  description = "The name of the project"
+  type        = string
+}
+
+variable "environment" {
+  description = "The environment for the VPC"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "The CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnets_cidrs" {
+  description = "The CIDR blocks for the public subnets"
+  type        = list(string)
+}
+
+variable "private_subnets_cidrs" {
+  description = "The CIDR blocks for the private subnets"
+  type        = list(string)
+}
+
+variable "availability_zones" {
+  description = "The availability zones for the VPC"
+  type        = list(string)
+}
